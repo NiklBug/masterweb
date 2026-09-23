3002726b7385b35151db103d8a63a2d3ddf3009f
nikl <119941658@qq.com>
2026-09-23 14:21:24 +0800

    no message

--- assets/main/index.b554b.js
+++ assets/main/index.b554b.js
@@ -36958,7 +36958,6 @@ window.__require = function e(t, n, r) {
     var GameConfig_1 = require("./../../platform/GameConfig");
     var AppConfig_1 = require("./../../platform/AppConfig");
     var VV_1 = require("./../../frameworks/VV");
-    var SceneManager_1 = require("../../frameworks/SceneManager");
     var LoginWrapper_1 = require("../login/LoginWrapper");
     var I18n_1 = require("../../frameworks/components/i18n/I18n");
     var PayMgr_1 = require("../pay/PayMgr");
@@ -37065,7 +37064,7 @@ window.__require = function e(t, n, r) {
             VV_1.vv.enterGameMgr.init();
             LoginWrapper_1.default.init();
             BIWrapper_1.BIWrapper.init();
-            SceneManager_1.default.getInstance().loadBundleScene(GameConst_1.GameBundle.Login, GameConfig_1.GameConfig.loginScene);
+            this.testH5();
             return [ 2 ];
           });
         });
@@ -37311,7 +37310,6 @@ window.__require = function e(t, n, r) {
     "../../base/common/http/XMLHttpRequestWrapper": "XMLHttpRequestWrapper",
     "../../base/common/utils/StringUtil": "StringUtil",
     "../../common/GameConst": "GameConst",
-    "../../frameworks/SceneManager": "SceneManager",
     "../../frameworks/components/analysis/AnalysisEvent": "AnalysisEvent",
     "../../frameworks/components/hotupdate/HotUpdateComponent": "HotUpdateComponent",
     "../../frameworks/components/hotupdate/HotUpdateEvent": "HotUpdateEvent",
@@ -40532,9 +40530,7 @@ window.__require = function e(t, n, r) {
         VV_1.vv.uiMgr.hideLoading();
         if (data && data.callback_url) {
           this._isRecharging = true;
-          VV_1.vv.logger.log("window.SystemOperation;", window["SystemOperation"]);
-          window["SystemOperation"] && window["SystemOperation"].openUrl && window["SystemOperation"].openUrl(data.callback_url);
-          VV_1.vv.logger.log("window.SystemOperation;", window.SystemOperation);
+          window.SystemOperation ? window.SystemOperation.openUrl(data.callback_url) : VV_1.vv.logger.log("h5 failed");
         } else VV_1.vv.toast.show("charge error:" + data.err_msg ? data.err_msg : data);
       };
       PayMgr.onWithDrawRsp = function(data) {
--- src/settings.7e680.js
+++ src/settings.7e680.js
@@ -30,6 +30,6 @@ window._CCSettings = {
         lobby: "965eb",
         login: "16be4",
         resources: "eb63a",
-        main: "9c36d"
+        main: "b554b"
     }
 };
